696c20ed13c495513f43e9e1831117783b69c0d7
thewebdeveloper <abdullah.webdeveloper@gmail.com>
2017-08-24 08:41:39 +0000

    Add blog entries feature, which allows the user to add blog entry from a web form

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 
 from . import app
 from .database import session, Entry
@@ -32,3 +32,19 @@ def entries(page=1):
         has_next = has_next
     )
     
+    
+@app.route("/entry/add", methods=["GET"])
+def add_entry_get():
+    return render_template("add_entry.html")
+    
+    
+@app.route("/entry/add", methods=["POST"])
+def add_entry_post():
+    entry = Entry(
+        title = request.form["title"],
+        content = request.form["content"],
+    )
+    session.add(entry)
+    session.commit()
+    
+    return redirect(url_for("entries"))
